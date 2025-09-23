a099e1e9f611793d7125eaf4b638ede8eb794b05
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-09-23 22:16:04 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 # arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     """
     Perform basic arithmetic operations.
 
     Parameters:
-        num1 (float): The first number.
-        num2 (float): The second number.
+        num1: The first number.
+        num2: The second number.
         operation (str): The operation to perform. Options: 'add', 'subtract', 'multiply', 'divide'.
 
     Returns:
@@ -24,3 +24,4 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 / num2
     else:
         return "Error: Invalid operation"
+
